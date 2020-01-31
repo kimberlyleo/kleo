7630a35e3aae3bf76aa797d2eb4b6c3337ed3f8b
kimberlyleo <kimeleonard@gmail.com>
2020-01-31 11:09:45 -0800

    pushing portfolio files

---
+++ precache-manifest.2cd0a5018966739cadae8fc664a2435a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a503722afa3a3280ae18",
+    "url": "/portfolio/static/css/main.0681d78d.chunk.css"
+  },
+  {
+    "revision": "a503722afa3a3280ae18",
+    "url": "/portfolio/static/js/main.a503722a.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "fea86e1ef3341e9265e1d273cfb45b88",
+    "url": "/portfolio/index.html"
+  }
+];
